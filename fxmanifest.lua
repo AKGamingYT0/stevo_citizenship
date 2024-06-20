ddd8078dbfb7a02bc372e68f2f2dfa9e388d9148
steve <billypeterlynch@outlook.com>
2024-06-20 13:20:36 +1000

    create config

---
+++ fxmanifest.lua
@@ -0,0 +1,37 @@
+fx_version "cerulean"
+game "gta5"
+
+author "StevoScripts"
+description "Citizen System."
+version "1.0.0"
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'modules/bridge/client/**.lua',
+    'modules/citizenship/client.lua',
+    'modules/interactions/**.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'modules/bridge/server/*.lua',
+    'modules/citizenship/server.lua',
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    '/server:4500',
+}
+   
+
+lua54 "yes"
+
+
+
+
+
